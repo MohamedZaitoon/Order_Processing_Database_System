38fb1432db8c260dfa5f328b3345388716a02a07
alkaz <alkaz@DESKTOP-7PNJLJR>
2020-05-17 20:34:06 +0200

    adding the profile editting window.

---
+++ src/controllers/Edit_profile_controller.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Edit_profile_controller {
+
+    @FXML
+    private TextField user_name_feild;
+
+    @FXML
+    private TextField first_name_feild;
+
+    @FXML
+    private TextField last_name_feild;
+
+    @FXML
+    private Button discard;
+
+    @FXML
+    private Button confirm;
+
+    @FXML
+    private Label error;
+
+}
--- src/controllers/profile_controller.java
+++ src/controllers/profile_controller.java
@@ -3,8 +3,21 @@ package controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.fxml.Initializable;
+import utils.ConnectionUtil;
+import utils.StatusUtil;
+import utils.User;
+import utils.StatusUtil.Status;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class profile_controller {
+public class profile_controller implements Initializable {
 
     @FXML
     private Label user_name;
@@ -24,4 +37,24 @@ public class profile_controller {
     @FXML
     private Button editBt;
 
+    private Connection connection = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+    
+    private User user; 
+    
+    
+    @Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+    
+    
+    
+    
+    
+    public void setUser(User user) {
+    	if (user != null)
+    		this.user = user;
+    }
 }
