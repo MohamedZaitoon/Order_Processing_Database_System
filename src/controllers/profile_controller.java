9f2b241ebca52c2aef37df0689eb60dbef988172
alkaz <alkaz@DESKTOP-7PNJLJR>
2020-05-17 17:51:33 +0200

    create controller for profile window.

---
+++ src/controllers/profile_controller.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class profile_controller {
+
+    @FXML
+    private Label user_name;
+
+    @FXML
+    private Label first_name;
+
+    @FXML
+    private Label last_name;
+
+    @FXML
+    private Label Email;
+
+    @FXML
+    private Label gender;
+
+    @FXML
+    private Button editBt;
+
+}
